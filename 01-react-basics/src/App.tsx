d9a0d6005504e51076b3b3dc224c7fe898c75602
Sofia Mattiasson <mattiasson.sofia@gmail.com>
2023-04-25 13:42:50 +0200

    Add a click statefull variabel

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -1,13 +1,28 @@
+import { useState } from 'react'
 import './App.css'
+// Allt i react är uppbbyggt med HOOKS
 
 const App = () => {
+
+    // Stateful variabel
+    // Array destructer 
+    const [ msg, setMsg ] = useState<string>("Hi you, im't stateful!")
+    const [ clicks, setClicks ] = useState<number>(0)
+
+    // let msg = 'Welcome to my application'
+    // console.log(msg);
+
+    const handleBtnClick = () => {
+        setClicks(clicks + 1)
+    }
 
   return (
     <div className="App">
         <h1>React basic</h1>
-        <h2>Message</h2>
-        <p>U have cliked the button Zero times</p>
-        <button className='btn btn-success btn-lg'> Click Me!</button>
+        <h2>{msg}</h2>
+        <p>U have cliked the button {clicks} times </p>
+
+        <button className='btn btn-success btn-lg' onClick={handleBtnClick}> Click Me!</button>
     </div>
   )
 }
