68f348cbc8e836f029d7b920ebe800878d52fe0f
Sofia Mattiasson <mattiasson.sofia@gmail.com>
2023-04-25 14:34:48 +0200

    Add array and mapping over it, and a key

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -1,6 +1,12 @@
+// Allt i react är uppbbyggt med HOOKS
 import { useState } from 'react'
 import './App.css'
-// Allt i react är uppbbyggt med HOOKS
+
+type Post = {
+    id: number,
+    title: string,
+    likes: number,
+}
 
 const App = () => {
 
@@ -8,6 +14,15 @@ const App = () => {
     // Array destructer 
     const [ msg, setMsg ] = useState<string>("Hi you, im't stateful!")
     const [ clicks, setClicks ] = useState<number>(0)
+
+    // Behöber sätta en Array of Post
+    const [ post, setPost ] = useState<Post[]>([
+        {id: 1, title: "React rocks 🤘🏻", likes: 1337},
+        {id: 2, title: "JSX rocks even more 🤘🏻", likes: 999},
+        {id: 3, title: "Got state?", likes: 1337},
+    ]) // Det är helt ok med vanliga variabler om man inte vill att dem ska överleva rendeingarna
+
+
 
     // let msg = 'Welcome to my application'
     // console.log(msg);
@@ -23,6 +38,17 @@ const App = () => {
         <p>U have cliked the button {clicks} times </p>
 
         <button className='btn btn-success btn-lg' onClick={handleBtnClick}> Click Me!</button>
+
+        <button className='btn btn-warning btn-lg' onClick={() => {setMsg("Hey you")}}> Hey you!</button>
+
+        <h2>Post</h2>
+        <ul>
+            { post.map( post => (
+                //Måste skriva när du har lista av array och ska mappa över så måset det finns en key (nucker). När man kör map så måste det fösta elementet i ha en key: Så den första föräldren inut map()
+            <li key={post.id}>{post.title} ({post.likes})</li>
+            ))
+         }
+        </ul>
     </div>
   )
 }
