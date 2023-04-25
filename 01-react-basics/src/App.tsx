b66c652987bd56d1cd8d63e5f53249760d82a367
Sofia Mattiasson <mattiasson.sofia@gmail.com>
2023-04-25 10:58:24 +0200

    Install dependencies and started coding very basic

---
+++ 01-react-basics/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="App">
+        <h1>React basic</h1>
+        <h2>Message</h2>
+        <p>U have cliked the button Zero times</p>
+        <button className='btn btn-success btn-lg'> Click Me!</button>
+    </div>
+  )
+}
+
+export default App
