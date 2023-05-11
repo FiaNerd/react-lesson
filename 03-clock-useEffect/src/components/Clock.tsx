a55ab87b71a347457ce1f78a0d2700b163c1285a
Sofia Mattiasson <mattiasson.sofia@gmail.com>
2023-05-11 10:41:58 +0200

    Cleanup function for sideeffects userEffect()

--- 03-clock-useEffect/src/components/Clock.tsx
+++ 03-clock-useEffect/src/components/Clock.tsx
@@ -6,14 +6,25 @@ const Clock = () => {
         console.log("I'm initing");
         return new Date().toLocaleTimeString()}) // En funktion i useState()
 
-        // Här startar vi upp timerns
+       
     useEffect(() => {
-        console.log("Starting clock");
-
-        setInterval(() => {
+        console.log("Clock is mounted. Tsarting clock");
+         // Här startar vi upp timerns
+        // This will only be executed when the componen it mounted
+        // and only AFTER the component has been render
+        const intervalId = setInterval(() => {
             setTime(new Date().toLocaleTimeString())
             console.log("tick")
         }, 1000 )
+
+        {/* Stoppar klockan genom att returnera den, så behöver inte kalla på den. Så endast retunerar en funktion som aldrig kallas på. Retunerar en funktion */}
+        return () => {
+            // This cleanup funtion will be executed when 
+            // the component is about to be unmounted
+            console.log("Clock is beeing unmonthed. Stopping timer")
+            clearInterval(intervalId)
+            console.log(intervalId);
+        } 
     },[] ) 
 
     // userEffect körs efetr den har renderat färdigt 
