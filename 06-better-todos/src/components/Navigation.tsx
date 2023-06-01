7fb806cf30153262d5c6764cd00c64530df3b1ac
Sofia Mattiasson <mattiasson.sofia@gmail.com>
2023-06-01 09:50:02 +0200

    Add warning and success for todo create

--- 06-better-todos/src/components/Navigation.tsx
+++ 06-better-todos/src/components/Navigation.tsx
@@ -14,7 +14,10 @@ const Navigation = () => {
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
 						<Nav.Link as={NavLink} to="/">Home</Nav.Link>
-						<Nav.Link as={NavLink} to="/todos">Todos</Nav.Link>
+                    {/* end = används för att man inte ska kunna ha hela todos active, det är för att både url böjrar 
+                    ned /todos så då blir båda aktiva även om det är /todos/create , så todos ska inte vara aktiv bara för att 
+                    det är samma url i början*/}
+						<Nav.Link as={NavLink} to="/todos" end>Todos</Nav.Link>
 						<Nav.Link as={NavLink} to="/todos/create">Create Todo</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
--- 06-better-todos/src/pages/CreateTodoPage.tsx
+++ 06-better-todos/src/pages/CreateTodoPage.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Todo } from '../types'
 import AddNewTodoForm from '../components/AddNewTodoForm'
 import * as TodosAPI from '../services/TodosAPI'
+import { Alert } from 'react-bootstrap'
 
 const CreateTodoPage = () => {
 	const [success, setSuccess] = useState<boolean | null>(null)
@@ -9,9 +10,16 @@ const CreateTodoPage = () => {
 	
 	// Create a new todo in the API
 	const addTodo = async (todo: Todo) => {
-		await TodosAPI.createTodo(todo)
+        try {
+            const createdTodo = await TodosAPI.createTodo(todo)
 
-		//Check result and set seuccess
+            // KOnventerar om createdTodo till boolan via två !! utropstecken
+            setSuccess(!!createdTodo)
+
+            // Es-lint.-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (err: any) {
+            setSuccess(false)
+        }
 	}
 
 	return (
@@ -19,6 +27,14 @@ const CreateTodoPage = () => {
 			<h1 className="mb-3">Create a new Todo</h1>
 
 			<AddNewTodoForm onAddTodo={addTodo} />
+
+            {success === true && (
+                <Alert variant="success" className='mt-3'>Todo Created</Alert>
+            )}
+
+            {success === false && (
+                <Alert variant="warning" className='mt-3'>Todo Could Not Be Created</Alert>
+            )}
 		</>
 	)
 }
