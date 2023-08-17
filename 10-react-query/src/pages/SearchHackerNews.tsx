a22de195357f49e2d32913c7c1a4ccb7549d39db
Sofia Mattiasson <mattiasson.sofia@icloud.com>
2023-08-17 15:08:06 +0200

    add search state to the search

--- 10-react-query/src/pages/SearchHackerNews.tsx
+++ 10-react-query/src/pages/SearchHackerNews.tsx
@@ -1,16 +1,27 @@
-import { Button, Form } from "react-bootstrap";
-
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { Alert, Button, Form, ListGroup } from "react-bootstrap";
+import { search } from "../services/TheHackerNewsApi";
 
 const SearchHackerNews = () => {
+    const [searchResult, setSearchResult] = useState("")
+    
+    const { data, error, isLoading, isFetching, refetch } = useQuery({
+        queryKey: ['search-news', `search?${searchResult}`],
+        queryFn: () => search(searchResult)
+    }) 
+
+    console.log(searchResult)
   return (
     <>
       <h1>Search 🔎</h1>
-      <Form className="mb-4">
+      <Form className="mb-4" onSubmit={() => refetch()}>
         <Form.Group className="mb-4">
             <Form.Control 
                 type="text" 
                 placeholder="Search for news" 
                 required
+                onChange={e => setSearchResult(e.target.value)}
             >
             </Form.Control>
         </Form.Group>
@@ -19,11 +30,37 @@ const SearchHackerNews = () => {
 				<Button
 					variant="success"
 					type="submit"
+                    disabled={isFetching}
 				>
-                Search
+                 {isFetching ? "Searching..." : "Search"}
                 </Button>
 			</div>
       </Form>
+
+      {error && <Alert variant='warning'>Not working</Alert>}
+
+			{isLoading && <p>🤔 Loading...</p>}
+
+			{searchResult && (
+				<div id="search-result">
+					<p>Showing {data?.nbHits} search results for "{data?.hitsPerPage}"pages...</p>
+
+		<ListGroup className="mb-3">
+			{data?.hits.map(hit => (
+			<ListGroup.Item
+				action
+				href={hit.url}
+				key={hit.objectID}
+			>
+				<h2 className="h3">{hit.title}</h2>
+					<p className="text-muted small mb-0">
+						{hit.points} points by {hit.author} at {hit.created_at}
+					</p>
+				</ListGroup.Item>
+			))}
+		</ListGroup>
+        </div>
+		)}
     </>
   );    
 };
---
+++ 10-react-query/src/services/TheHackerNewsApi.ts
@@ -0,0 +1,62 @@
+/**
+ * Hacker News API service
+ *
+ * <https://hn.algolia.com/api>
+ */
+
+import axios from 'axios'
+import { HN_SearchResponse } from '../types/TheHackerNewsApi.types'
+
+const FAKE_DELAY = 1500;
+
+// Create a new axios instance
+const instance = axios.create({
+	baseURL: "https://hn.algolia.com/api/v1",
+	timeout: 10000,
+
+    // Verkar inte behövas i denna
+	// headers: {
+	// 	"Content-Type": "application/json",
+	// 	"Accept": "application/json"
+	// }
+})
+
+/**
+ * Execute a HTTP GET request to an endpoint.
+ *
+ * @param {string} endpoint Endpoint to HTTP GET
+ * @returns Promise
+ */
+const get = async <T>(endpoint: string) => {
+	const response = await instance.get(endpoint)
+
+    !!FAKE_DELAY && await new Promise(resolve => setTimeout(resolve, FAKE_DELAY))
+
+	return response.data as T
+}
+
+/**
+ * Search Hacker News stories
+ *
+ * @todo Replace any with correct type definition 😱!
+ *
+ * @param {string} query Search query to search for
+ * @param {number} page Page of search results to get
+ * @returns Promise
+ */
+export const search = async (query: string, page = 1) => {
+	return get<HN_SearchResponse>(`/search?query=${query}&tags=story&page=${page}`)
+}
+
+/**
+ * Search Hacker News stories by date
+ *
+ * @todo Replace any with correct type definition 😱!
+ *
+ * @param {string} query Search query to search for
+ * @param {number} page Page of search results to get
+ * @returns Promise
+ */
+export const searchByDate = async (query: string, page = 1) => {
+	return get<HN_SearchResponse>(`/search_by_date?query=${query}&tags=story&page=${page}`)
+}
