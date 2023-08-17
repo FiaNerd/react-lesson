184d9c69ab37fe932428c33be10bc10a078fb293
Sofia Mattiasson <mattiasson.sofia@icloud.com>
2023-08-17 13:43:17 +0200

    Add form to search for hacker news

--- 10-react-query/src/pages/SearchHackerNews.tsx
+++ 10-react-query/src/pages/SearchHackerNews.tsx
@@ -1,7 +1,29 @@
+import { Button, Form } from "react-bootstrap";
+
+
 const SearchHackerNews = () => {
   return (
     <>
       <h1>Search 🔎</h1>
+      <Form className="mb-4">
+        <Form.Group className="mb-4">
+            <Form.Control 
+                type="text" 
+                placeholder="Search for news" 
+                required
+            >
+            </Form.Control>
+        </Form.Group>
+
+            <div className="d-flex justify-content-end">
+				<Button
+					variant="success"
+					type="submit"
+				>
+                Search
+                </Button>
+			</div>
+      </Form>
     </>
   );    
 };
